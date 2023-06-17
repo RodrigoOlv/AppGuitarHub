a6101ffb023c7279b108f00d2aab8190f33a12c7
Rodrigo Oliveira <rodrigo@santins.com.br>
2023-06-17 00:36:56 -0300

    Refactor listing imports

--- app/src/main/java/com/example/appguitarhub/ui/listing/ListingFragment.java
+++ app/src/main/java/com/example/appguitarhub/ui/listing/ListingFragment.java
@@ -1,15 +1,14 @@
 package com.example.appguitarhub.ui.listing;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.example.appguitarhub.R;
 import com.example.appguitarhub.model.Equipment;
